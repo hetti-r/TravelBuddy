8a820990150a9b45eea9c8d67c78f6c2bd348795
Hetti <hetti.ronnemaa@gmail.com>
2024-06-05 18:44:48 +0300

    add browse and single page styling

--- src/Controller/SinglePageController.php
+++ src/Controller/SinglePageController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class SinglePageController extends AbstractController
 {
-    #[Route('/single/page', name: 'app_single_page')]
+    #[Route('/singlepage', name: 'app_single_page')]
     public function index(): Response
     {
         return $this->render('single_page/index.html.twig', [
